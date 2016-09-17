987caa8571a9d4d180fb875185e5cfc28df12243
Alex Fakhri <alexfakhri@me.com>
2016-09-17 12:42:27 +0100

    Added power saving mode

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -33,10 +33,14 @@ describe('Thermostat', function(){
     expect(thermostat.getCurrentTemperature()).toEqual(10);
   });
 
+  // Power saving mode is on by default
+  it('has power saving mode on be deafualt', function(){
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
 });
 
 // If power saving mode is on, the maximum temperature is 25 degrees
 // If power saving mode is off, the maximum temperature is 32 degrees
-// Power saving mode is on by default
 // You can reset the temperature to 20 by hitting the reset button
 // The thermostat should color the display based on energy usage - < 18 is green, < 25 is yellow, otherwise red

d2c8b7cc0eb01813230ae0a382a49adb9f4e446d
Alex Fakhri <alexfakhri@me.com>
2016-09-18 18:34:57 +0100

    Added JQuery to power saver buttons

--- src/interface.js
+++ src/interface.js
@@ -15,3 +15,20 @@ $('#temperature-down').click(function() {
   thermostat.decreaseTemp();
   updateTemperature();
 });
+
+$('#temperature-reset').click(function() {
+ thermostat.resetTemp();
+ updateTemperature();
+});
+
+$('#powersaving-on').click(function() {
+  thermostat.switchOnPowerSavingMode();
+  $('#power-saving-status').text(' on')
+  updateTemperature();
+});
+
+$('#powersaving-off').click(function() {
+  thermostat.switchOffPowerSavingMode();
+  $('#power-saving-status').text(' off')
+  updateTemperature();
+});
